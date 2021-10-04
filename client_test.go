dd20d03b08f29f222f90ce9b871772f727ff3c3a
mrz1836 <mrz1818@gmail.com>
2021-10-04 11:11:33 -0400

    Bumped version

--- client_test.go
+++ client_test.go
@@ -187,7 +187,7 @@ func ExampleNewClient() {
 		return
 	}
 	fmt.Printf("loaded client: %s", client.options.userAgent)
-	// Output:loaded client: go-tonicpow: v0.6.13
+	// Output:loaded client: go-tonicpow: v0.6.14
 }
 
 // BenchmarkNewClient benchmarks the method NewClient()
